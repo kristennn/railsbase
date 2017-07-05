01b9687cdcfb936989fef0bc1ae57f661c9c8bcd
13949836783@163.com <13949836783@163.com>
2017-07-05 23:04:43 +0800

    final commit

---
+++ 15prictice/third_question.rb
@@ -0,0 +1,5 @@
+wday = {:sunday => "星期天", :monday => "星期一", :saturday => "星期六", :thusday => "星期二", :wednesday => "星期三", :thursday => "星期四", :friday => "星期五"}
+
+%w(sunday monday saturday thusday wednesday thursday friday).each do |day|
+  puts "“#{}”"
+end
